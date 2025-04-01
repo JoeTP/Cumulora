3400843315f8fbb611f7e9cd84bd7080db6f28a9
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-04-01 14:15:03 +0200

    cp

--- app/src/main/java/com/example/cumulora/data/responsestate/WeatherStateResponse.kt
+++ app/src/main/java/com/example/cumulora/data/responsestate/WeatherStateResponse.kt
@@ -11,14 +11,3 @@ sealed class WeatherStateResponse {
     data class Failure(val error: String) : WeatherStateResponse()
 }
 
-sealed class CombinedStateResponse {
-
-    object Loading : CombinedStateResponse()
-
-    data class Success(
-        val weather: WeatherStateResponse.Success,
-        val forecast: ForecastStateResponse.Success
-    ) : CombinedStateResponse()
-
-    data class Failure(val error: String) : CombinedStateResponse()
-}
